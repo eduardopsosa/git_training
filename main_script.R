cc88ed97726cefada2c325ed831f1b9338f2443b
eduardopsosa <eduardo.perez@unizar.es>
2026-06-11 13:00:14 +0200

    started cool analysis with setosa

--- main_script.R
+++ main_script.R
@@ -1,4 +1,13 @@
 library(dplyr)
 library(ggplot2)
+library(readr)
 
-iris_data <- iris
+# reading data
+iris_data <- read_csv('raw_data/iris.csv')
+
+# subsetting data
+setosa_subset <- iris_data |>
+  filter(Species == "setosa")
+
+# cool analysis
+model_setosa <- lm(Sepal.Length ~ Petal.Length, data = setosa_subset)
